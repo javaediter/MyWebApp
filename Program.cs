2e1eb5dea75e7c3a0c725989db240d997ed8545d
Edison Teran <javaedi.et@gmail.com>
2026-09-17 14:04:59 -0500

    Add .env file and update pipeline

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+DotNetEnv.Env.Load();
+var environment = Environment.GetEnvironmentVariable("APP_ENV");
+Console.WriteLine($"-----> environment is {environment}");
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
